d8a4ce33658131a2a1a225a290f664683fae2312
Joel Wetzel <jwetzel@affirma.com>
2021-05-13 10:24:36 -0500

    Adjust engine sound thresholds.

--- LocoSounds/include/locoTypes/alco_rs3.h
+++ LocoSounds/include/locoTypes/alco_rs3.h
@@ -4,7 +4,7 @@
 
 // RS-3 specific figures
 #define ENGINE_RPM_IDLE         315
-#define ENGINE_RPM_1            420
+#define ENGINE_RPM_1            340
 #define ENGINE_RPM_2            580
 #define ENGINE_RPM_MAX          1000
 
